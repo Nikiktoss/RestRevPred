224e759bd8dad26440042e5f52f7d36c4cb6285f
Nikita Chebotarevskiy <mikita.chebotarevskiy@gmail.com>
2023-04-08 15:16:27 +0300

    add file fields for PredictionResult model

--- rrp/models.py
+++ rrp/models.py
@@ -3,6 +3,14 @@ from django.db import models
 from django.utils.text import slugify
 
 
+def user_directory_path_pdf(instance, filename):
+    return 'pdf_files/user_{0}/{1}'.format(instance.user.id, filename)
+
+
+def user_directory_path_json(instance, filename):
+    return 'json_files/user_{0}/{1}'.format(instance.user.id, filename)
+
+
 class User(AbstractUser):
     photo = models.ImageField(upload_to="media/user_pictures/%Y-%m-%d/", verbose_name="Account photo", blank=True)
     creation_date = models.DateField(auto_now_add=True, verbose_name="Date of creation", blank=True)
@@ -22,3 +30,5 @@ class PredictionResult(models.Model):
     user = models.ForeignKey(User, verbose_name="User", on_delete=models.CASCADE)
     revenue = models.DecimalField(verbose_name="Predict revenue",  max_digits=20, decimal_places=9)
     date_of_calculations = models.DateField(verbose_name="Date of calculations", auto_now_add=True)
+    pdf_file = models.FileField(verbose_name="PDF File", upload_to=user_directory_path_pdf, blank=True, null=True)
+    json_file = models.FileField(verbose_name="JSON File", upload_to=user_directory_path_json, blank=True, null=True)
